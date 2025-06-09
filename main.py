22c810c2448438530a2018b73a9bb6282f28680f
kimmin12345 <kkmss8880@shs.hs.kr>
2025-06-09 10:59:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import streamlit as st
 st.title("첫 app")
-st.write("hello")
-a=st.text_input("이름을 입력하시오")
-if a:
-  if a=="홍길동":
+name=st.text_input("이름을 입력하시오")
+if name:
+  if name=="홍길동":
     st.success("반갑습니다.")
   else:
     st.warning("누구세요")
